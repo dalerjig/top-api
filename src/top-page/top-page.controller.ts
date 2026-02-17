d27f84b1bc0fb7d82cdeda09b3cd2c63fd43034d
dalerjig <dalerjig@shtab.tech>
2026-02-17 05:58:00 +0300

    controllers created

---
+++ src/top-page/top-page.controller.ts
@@ -0,0 +1,33 @@
+import {
+  Controller,
+  Post,
+  Body,
+  Param,
+  HttpCode,
+  Patch,
+  Delete,
+  Get,
+} from '@nestjs/common';
+import { TopPageModel } from './top-page.model';
+import { FindTopPageDto } from './dto/find-top-page.dto';
+
+@Controller('top-page')
+export class TopPageController {
+  @Post('create')
+  // Omit is function let exclude id
+  async create(@Body() dto: Omit<TopPageModel, '_id'>) {}
+
+  @Get(':id')
+  async get(@Param('id') id: string) {}
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {}
+
+  @Patch(':id')
+  //here needs full dto
+  async patch(@Param('id') id: string, @Body() dto: TopPageModel) {}
+
+  @HttpCode(200)
+  @Post()
+  async find(@Body() dto: FindTopPageDto) {}
+}
